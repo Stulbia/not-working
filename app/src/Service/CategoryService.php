7f698cb8b2fc27234acc71f1650facca8008be0f
Twoje Imię Nazwisko <toja@example.com>
2024-05-13 11:03:26 +0200

    zajecia2

---
+++ app/src/Service/CategoryService.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Category service.
+ */
+
+namespace App\Service;
+
+use App\Repository\CategoryRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
+/**
+ * Class CategoryService.
+ */
+class CategoryService implements CategoryServiceInterface
+{
+    /**
+     * Items per page.
+     *
+     * Use constants to define configuration options that rarely change instead
+     * of specifying them in app/config/config.yml.
+     * See https://symfony.com/doc/current/best_practices.html#configuration
+     *
+     * @constant int
+     */
+    private const PAGINATOR_ITEMS_PER_PAGE = 10;
+
+    /**
+     * Constructor.
+     *
+     * @param CategoryRepository     $categoryRepository Categoryrepository
+     * @param PaginatorInterface $paginator      Paginator
+     */
+    public function __construct(private readonly CategoryRepository $categoryRepository, private readonly PaginatorInterface $paginator)
+    {
+    }
+
+    /**
+     * Get paginated list.
+     *
+     * @param int $page Page number
+     *
+     * @return PaginationInterface<string, mixed> Paginated list
+     */
+    public function getPaginatedList(int $page): PaginationInterface
+    {
+        return $this->paginator->paginate(
+            $this->categoryRepository->queryAll(),
+            $page,
+            self::PAGINATOR_ITEMS_PER_PAGE
+        );
+    }
+
+    public function save(Category $category): void
+    {
+        assert($this->_em instanceof EntityManager);
+        $this->_em->persist($category);
+        $this->_em->flush();
+    }
+}
